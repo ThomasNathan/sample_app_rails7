f2945ed64cd22e20f143b9348291c65edf5293c6
Nathan Thomas <thomas.nathanm@gmail.com>
2022-09-23 09:28:39 -0500

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -61,4 +61,8 @@ module SessionsHelper
     @current_user = nil
   end
 
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,12 +9,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
     # View section 5.3.4 for information about this test
     # Not sure if test is necessary since he provides an alterate option
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    user = users(:michael)
+    log_in_as(user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
   end
 
 end
