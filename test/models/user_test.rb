d873bed7c5a428667995deb9733457f8dc114537
Nathan Thomas <thomas.nathanm@gmail.com>
2022-09-27 17:52:47 -0500

    Follow and unfollow users, add turbo stream

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,7 +80,21 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
-      @user.destroy 
+      @user.destroy
     end
+  end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+    # users can't follow themselves.
+    michael.follow(michael)
+    assert_not michael.following?(michael)
   end
 end
